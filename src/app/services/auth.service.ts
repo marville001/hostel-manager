a453f3ba478c06ef4791eeafce9b5fb462191b6c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-15 00:26:59 +0300

    Updates

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,28 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-interface User{
+interface User {
   email: string;
   password: string;
-  username?:string;
-  gender?:string;
+  username?: string;
+  gender?: string;
 }
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   public user: any;
-  private _registerUrl = "http://localhost:5000/api/v1/auth/signup";
-  private _loginUrl = "http://localhost:5000/api/v1/auth/login";
+  private _registerUrl = 'http://localhost:5000/api/v1/auth/signup';
+  private _loginUrl = 'http://localhost:5000/api/v1/auth/login';
 
   constructor(private router: Router, private http: HttpClient) {}
 
-  registerUser(user:User) {
-    return this.http.post<any>(this._registerUrl, user)
+  registerUser(user: User) {
+    return this.http.post<any>(this._registerUrl, user);
   }
 
   loginUser(user: User) {
-    return this.http.post<any>(this._loginUrl, user)
+    return this.http.post<any>(this._loginUrl, {
+      username: user.email,
+      password: user.password,
+    });
   }
 
   setToken(token: string): void {
