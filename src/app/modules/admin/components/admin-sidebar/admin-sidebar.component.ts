7f54d973987e23f3b875da6a58bec70b0340d155
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-16 20:11:37 +0300

    Logout user

--- src/app/modules/admin/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/modules/admin/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AppNavigationService } from 'src/app/services/app-navigation.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'admin-sidebar',
   templateUrl: './admin-sidebar.component.html',
 })
 export class AdminSidebarComponent implements OnInit {
-  constructor(public appNavigationService: AppNavigationService) {}
+  constructor(
+    public appNavigationService: AppNavigationService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {}
+
+  logoutUser() {
+    this.authService.logout();
+  }
 }
