b22b7242e6ba9b17336ad597995305c066989e5d
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-17 00:01:26 +0300

    Updates

---
+++ src/app/modules/admin/components/houses/vacant-houses/vacant-houses.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vacant-houses',
+  templateUrl: './vacant-houses.component.html',
+  styleUrls: ['./vacant-houses.component.scss']
+})
+export class VacantHousesComponent implements OnInit {
+  houses: any[] = [
+    {id:1, name:"Rongai Hostels", image: "https://images.pexels.com/photos/3958958/pexels-photo-3958958.jpeg", rooms: 5, full:false},
+    {id:3, name:"Aberdare Hostels", image: "https://images.pexels.com/photos/1358853/pexels-photo-1358853.jpeg", rooms: 12, full:false}
+  ]
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
