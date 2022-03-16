37cb4d7981136638bc7a305384b7fac7264c7ca8
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-16 19:51:21 +0300

    Add guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -8,11 +8,11 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
   canActivate(): boolean {
-    // if (this.authService.isLoggedIn()) {
+    if (this.authService.isLoggedIn()) {
       return true;
-    // } else {
-    //   this.router.navigate(['login']);
-    //   return false;
-    // }
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
   }
 }
